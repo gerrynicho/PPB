3478cc3177048fa13de9839a9affb67e8e4a90e8
Gerry Nicholas <gerrynicholas76@gmail.com>
2026-05-13 12:39:39 +0700

    fix: replace id with uuid

--- MarketplaceSiswa/app/src/main/java/com/example/marketplacesiswa/model/Product.kt
+++ MarketplaceSiswa/app/src/main/java/com/example/marketplacesiswa/model/Product.kt
@@ -1,7 +1,9 @@
 package com.example.marketplacesiswa.model
 
+import java.util.UUID
+
 data class Product(
-    val id: Long = System.currentTimeMillis(),
+    val id: String = UUID.randomUUID().toString(),
     val name: String,
     val price: String,
     val description: String
